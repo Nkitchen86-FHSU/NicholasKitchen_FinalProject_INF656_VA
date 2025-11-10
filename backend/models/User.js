49930e996f53be54699a09425bc6855371a45978
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-10 16:44:09 -0600

    Added user roles.

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
+    password: { type: String, required: true },
+
+    // User is either user or admin
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }    
+}, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
