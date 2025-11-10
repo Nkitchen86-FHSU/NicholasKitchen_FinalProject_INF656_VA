2d688a4ae0d08a962feb832a00c4c6c5f1409d70
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-09 22:56:48 -0600

    Added asset and auth routes.

---
+++ backend/routes/assetRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import Asset from "../models/Asset";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", protect, async(req, res) => res.json(await Asset.find()));
+router.post("/", protect, async(req, res) => res.json(await Asset.create(req.body)));
+router.put("/:id", protect, async(req, res) => res.json(await Asset.findByIdAndUpdate(req.params.id, req.body, { new: true })));
+router.delete("/:id", protect, async(req, res) => { await Asset.findByIdAndDelete(req.params.id); res.json({ message: "Deleted" }); });
+
+export default router;
