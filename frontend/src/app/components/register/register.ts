1c2ebdc60f19f726a78e5ce630574f3c35556a2f
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-10 18:57:02 -0600

    Updated register component.

--- frontend/src/app/components/register/register.ts
+++ frontend/src/app/components/register/register.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './register.css',
 })
 export class Register {
+  username = '';
+  password = '';
 
+  constructor(private auth: Auth, private router: Router) {}
+
+  onRegister() {
+    this.auth.register({ username: this.username, password: this.password }).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) => alert(err.error?.message || 'Registration failed'),
+    });
+  }
 }
