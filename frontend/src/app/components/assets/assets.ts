3f03ac1f1e7754b900d565d7f0721eeaa3b3b807
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-10 19:07:03 -0600

    Updated assets component.

--- frontend/src/app/components/assets/assets.ts
+++ frontend/src/app/components/assets/assets.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Asset } from '../../services/asset';
 
 @Component({
   selector: 'app-assets',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './assets.html',
   styleUrl: './assets.css',
 })
-export class Assets {
+export class Assets implements OnInit {
+  assets: any[] = [];
 
+  constructor(private assetService: Asset) {}
+
+  ngOnInit() {
+    this.loadAssets();
+  }
+
+  loadAssets() {
+    this.assetService.getAssets().subscribe({
+      next: (res) => this.assets = res as any[],
+      error: (err) => console.error('Failed to load assets', err)
+    });
+  }
+
+  deleteAsset(id: string) {
+    if (!confirm('Delete this asset?')) return;
+    this.assetService.deleteAsset(id).subscribe(() => this.loadAssets());
+  }
 }
