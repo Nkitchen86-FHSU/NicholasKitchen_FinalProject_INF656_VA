23b5921e7ff4045753734c6d6dfffe2323bb8e34
Tech Helpdesk <techdesk@salpublib.org>
2025-11-13 15:46:43 -0600

    Updated create asset modal. Updated asset table.

--- frontend/src/app/components/assets/assets.ts
+++ frontend/src/app/components/assets/assets.ts
@@ -1,27 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { Asset } from '../../services/asset';
+import { AssetService, Asset } from '../../services/asset';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
+import { FormsModule } from '@angular/forms';
+import { AdminService, User } from '../../services/admin';
 
 @Component({
   selector: 'app-assets',
-  imports: [CommonModule, Navbar],
+  imports: [CommonModule, Navbar, FormsModule],
   templateUrl: './assets.html',
   styleUrl: './assets.css',
 })
 export class Assets implements OnInit {
-  assets: any[] = [];
+  assets: Asset[] = [];
+  users: User[] = [];
+  newAsset = { name: '', category: '', serialNumber: '', assignedUser: '', purchaseDate: '', warranty: '', status: 'Active'};
 
-  constructor(private assetService: Asset) {}
+  constructor(
+    private assetService: AssetService, 
+    private adminService: AdminService
+  ) {}
 
   ngOnInit() {
     this.loadAssets();
+    this.loadUsers();
   }
 
   loadAssets() {
     this.assetService.getAssets().subscribe({
       next: (res) => this.assets = res as any[],
       error: (err) => console.error('Failed to load assets', err)
+    });
+  }
+
+  loadUsers() {
+    this.adminService.getAllUsers().subscribe({
+      next: (res) => this.users = res as any[],
+      error: (err) => console.error('Failed to load assets', err)
+    });
+  }
+
+  createAsset() {
+    this.assetService.createAsset(this.newAsset).subscribe(() => {
+      this.newAsset = { name: '', category: '', serialNumber: '', assignedUser: '', purchaseDate: '', warranty: '', status: 'Active'};
+      this.loadAssets();
     });
   }
 
